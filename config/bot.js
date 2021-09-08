6331b00262ac066f44e0931a529fa29609366bcd
JerodsonJ <73870090+MrGamingtor@users.noreply.github.com>
2021-09-08 09:56:21 +0400

    First

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODgyOTIwNTAxMDkyMTc1OTA1.YTCZyw.wPHHhYwK89-xlq_Vd7wXCCusf-0',
+        prefix: 'm?',
+        activity: 'Music | Prefix m?',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
